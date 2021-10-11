627fedf5fef3aa7d85b64959a869b6dfdf5789d2
Sandman <Sandman@163.com>
2021-10-11 16:49:53 +0800

    third master

--- src/main/java/com/atguigu/chapter02/Flink01_BatchWC.java
+++ src/main/java/com/atguigu/chapter02/Flink01_BatchWC.java
@@ -30,7 +30,7 @@ public class Flink01_BatchWC {
             public void flatMap(String value, Collector<Tuple2<String, Long>> out) throws Exception {
                 String[] words = value.split(" ");
                 for (String word : words) {
-                    out.collect(Tuple2.of(word, 2L));
+                    out.collect(Tuple2.of(word, 10L));
                 }
             }
         });
